62e04bf2cab99d1d070bb215f7c2caa4ef0e9412
Ahmed mohamed taha <ahmed.wesal03@eng-st.cu.edu.eg>
2024-05-11 05:10:40 +0300

    trying json and backend database

--- server.py
+++ server.py
@@ -1,29 +1,72 @@
-import redis 
+import redis
+import json
 
-redis_host = "localhost"
-redis_port = 6379
+redis_host='redis-17832.c281.us-east-1-2.ec2.redns.redis-cloud.com'
+redis_port = 17832
+password = "Hamza123"
+r = redis.Redis(redis_host,redis_port, db=0, password=password)
 
-def redis_string():
-    try:
-        r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-        r.set("msg", "Hello Redis!!!")
-        msg = r.get("msg")
-        print(msg)
-    except Exception as e:
-        print(e)
-        
-def redis_integer():
-    try:
-        r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-        r.set("counter", 100)
-        counter = r.get("counter")
-        r.incr("counter")
-        incr_counter = r.get("counter")
-        print(incr_counter)
-        print(counter)
-    except Exception as e:
-        print(e)
-        
-if __name__ == "__main__":
-    redis_string()
-    redis_integer() 
+
+def load_data_from_json(json_file):
+    with open(json_file) as f:
+        return json.load(f)
+
+def store_patients_and_doctors(patients, doctors):
+   
+    # Store patients
+    for patient in patients:
+        r.hmset(f"patient:{patient['id']}", patient)
+
+    # Store doctors
+    for doctor in doctors:
+        r.hmset(f"doctor:{doctor['id']}", doctor)
+
+    print("Patients and doctors stored in Redis.")
+    
+
+def get_patients_and_doctors():
+    patients = []
+    doctors = []
+
+    # Retrieve patients
+    patient_keys = r.keys("patient:*")
+    for key in patient_keys:
+        patient_data = r.hgetall(key)
+        patients.append({k.decode(): v.decode() for k, v in patient_data.items()})
+
+    # Retrieve doctors
+    doctor_keys = r.keys("doctor:*")
+    for key in doctor_keys:
+        doctor_data = r.hgetall(key)
+        doctors.append({k.decode(): v.decode() for k, v in doctor_data.items()})
+
+    return patients, doctors
+
+# Get patients and doctors from Redis
+patients, doctors = get_patients_and_doctors()
+
+# Print the retrieved data
+#print(json.dumps(patient, indent=4))
+
+
+
+def find_patient_by_name(name, patients):
+    for patient in patients:
+        if patient['name'] == name:
+            return patient
+    return None
+
+def find_doctor_by_specialty(specialty, doctors):
+    found_doctors = [doctor for doctor in doctors if doctor['specialty'] == specialty]
+    return found_doctors
+
+# Example: Retrieve patient by name
+patient_name = 'Bob'
+found_patient = find_patient_by_name(patient_name, patients)
+print(found_patient["name"])
+
+# Example: Retrieve doctors by specialty
+doctor_specialty = 'Neurology'
+found_doctors = find_doctor_by_specialty(doctor_specialty, doctors)
+for doctor in found_doctors:
+    print(doctor['name'])
