476bcc8b27d5fcf5dccab6859defaf28af2c847b
Ahmed mohamed taha <ahmed.wesal03@eng-st.cu.edu.eg>
2024-05-11 03:20:30 +0300

    linking GUI done

---
+++ app.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QTimer
+from login import *
+from signup import *
+from DoctorGUI import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.current_layout = None
+        self.setupUi("signup")
+        
+
+    def setupUi(self, layout_name):
+        if layout_name == "signup":
+            self.current_layout = SignUp()
+        elif layout_name == "login":
+            self.current_layout = LogIn()
+        elif layout_name == "Doctor":
+            self.current_layout = Ui_MainWindow()
+
+        self.current_layout.setupUi(self)
+        if layout_name == "signup":
+            self.current_layout.pushButton.clicked.connect(lambda: self.switch_layout("login"))
+            self.current_layout.signup.clicked.connect(lambda: self.switch_layout("Doctor"))
+        elif layout_name == "login":
+            self.current_layout.pushButton.clicked.connect(lambda: self.switch_layout("signup"))
+            self.current_layout.Login_button.clicked.connect(lambda: self.switch_layout("Doctor"))
+        elif layout_name == "Doctor":
+            self.current_layout.logoutButton.clicked.connect(lambda: self.switch_layout("login"))
+
+       
+    def switch_layout(self, layout_name):
+        # Clear existing layout
+        self.current_layout = None
+        self.centralWidget().deleteLater()  # Clear existing widgets
+        # Setup new layout
+        
+        self.setupUi(layout_name)
+        self.adjustSize()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
